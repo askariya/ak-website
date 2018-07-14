5d8df38a1f093fb7242c94d1a3897fb4c327c1a4
askariya <arunaluk@yahoo.ca>
2018-07-13 21:12:25 -0600

    Adding body around container and padding

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="Home">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col">
-          <div class="row justify-content-center">
-            <div class="col text-center">
-              <img id="intro_pic" src="../assets/arunalu.jpg" alt="arunalu"/>
-            </div>
-            <div class="w-100"></div>
-          <div class="row justify-content-center">
-            <div class="col">
+    <body>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col center-block">
+            <center>
+            <img id="intro_pic" src="../assets/arunalu.jpg" alt="arunalu"/>
+            <div class="information">
+              <h4><b>Information</b></h4>
               <ul>
                 <li> <b>Name:</b> Arunalu Kariyawasam</li>
                 <li> <b>Location:</b> Calgary, AB</li>
@@ -17,14 +15,14 @@
                 <li> <b>Email:</b> askariya@ucalgary.ca</li>
               </ul>
             </div>
+            </center>
           </div>
+          <div class="col-8" border="1px solid black">
+            <h1>{{ msg }}</h1>
           </div>
-        </div>
-        <div class="col-8" border="1px solid black">
-          <h1>{{ msg }}</h1>
         </div>
       </div>
-    </div>
+    </body>
   </div>
 </template>
 
@@ -33,7 +31,7 @@ export default {
   name: 'Home',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Summary'
     }
   }
 }
@@ -41,11 +39,23 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+body{
+  padding-top: 1em;
+}
+
 h1, h2 {
   font-weight: normal;
 }
+
+.information{
+  display: inline-block;
+  /* text-align: center; */
+}
 ul {
   list-style-type: none;
+  display: inline-block;
+  text-align: left;
   /* padding: 0; */
 }
 a {
@@ -54,21 +64,25 @@ a {
 
 img{
     /* border: 0.2em solid #3D5A80; */
-    /* margin-bottom: 1em; */
+    margin-bottom: 1em;
 }
 
 #intro_pic{
     /* border-radius: 15em; */
-    max-height: 20em;
+    max-height: 15em;
     width: auto;
 }
 
 /*debugging*/
 .col{
-  border: 1px solid black
+  border: 1px solid black;
 }
 .col-8{
-  border: 1px solid black
+  border: 1px solid black;
+}
+.container{
+  border: 1px solid red;
+  /* padding: 1em; */
 }
 
 </style>
