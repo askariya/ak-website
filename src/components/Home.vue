b6063e728389e18c97788bf9647b72d788228134
askariya <arunaluk@yahoo.ca>
2018-07-13 20:20:36 -0600

    Initial layout attempt

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="Home">
-  <h1>{{ msg }}</h1>
-
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col">
+          <div class="row justify-content-center">
+            <div class="col text-center">
+              <img id="intro_pic" src="../assets/arunalu.jpg" alt="arunalu"/>
+            </div>
+            <div class="w-100"></div>
+          <div class="row justify-content-center">
+            <div class="col">
+              <ul>
+                <li> <b>Name:</b> Arunalu Kariyawasam</li>
+                <li> <b>Location:</b> Calgary, AB</li>
+                <li> <b>Phone:</b> 587-707-4129</li>
+                <li> <b>Email:</b> askariya@ucalgary.ca</li>
+              </ul>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div class="col-8" border="1px solid black">
+          <h1>{{ msg }}</h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,13 +46,29 @@ h1, h2 {
 }
 ul {
   list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
+  /* padding: 0; */
 }
 a {
   color: #42b983;
 }
+
+img{
+    /* border: 0.2em solid #3D5A80; */
+    /* margin-bottom: 1em; */
+}
+
+#intro_pic{
+    /* border-radius: 15em; */
+    max-height: 20em;
+    width: auto;
+}
+
+/*debugging*/
+.col{
+  border: 1px solid black
+}
+.col-8{
+  border: 1px solid black
+}
+
 </style>
